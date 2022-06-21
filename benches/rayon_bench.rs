bb84c5a3a64544b6501036c9c15e5c78750801fe
GuestMeNot <nadine_m_mckenzie@yahoo.com>
2022-06-21 11:40:37 -0400

    rayon bench docs.

--- benches/rayon_bench.rs
+++ benches/rayon_bench.rs
@@ -22,31 +22,36 @@ mod tests {
     /// **NOTE**: The following points illustrate that using parallel processing to increase
     /// performance of the [MerkleTree](crate::MerkleTree) is tricky:
     ///
-    /// 1. Rayon's `par_iter` does not work well with slices of undefined length.
+    /// 1. Rayon's `par_iter` does not work well with slices of undefined length. It works well with
+    ///    `Vec`s and fixed size arrays.
     ///
-    /// 2. Passing a `&Vec` to create the `MerkleTree` rather than a slice or `Iter` dramatically
-    ///    decreased performance when using Rayon's `par_iter()` to create the [MerkleTree] leaf
-    ///    hashes compared to a simple `for` loop
+    /// 2. Changing `MerkleTree::new` parameter from slice and `Iter` to `&Vec`dramatically
+    ///    decreased performance when using Rayon's `par_iter()` to hash the leaves.
     ///
-    /// 2. Creating a generic `[T; N]` using a `const` generic parameter to satisfy Rayon's `par_iter`
-    ///    traits requires the caller to know the exact number of leaves at compile time.
+    /// 3. Since Rayon's `par_iter` will work with fixed sized arrays, we can change the `MerkleTree::new`
+    ///    parameter to a generic `[T; N]` by adding a `const` generic parameter, but this requires
+    ///    the caller to know the exact number of leaves at compile time.
     ///
-    /// 3. Using Rayon's `par_bridge` leaves reorders the results of the original data.
+    /// 4. Using Rayon's `par_bridge` reorders the results compared to the original leaf indices.
+    ///    This has the advantage of working with exist fn parameters. The disadvantage is that
+    ///    `par_bridge` does not keep the original ordering of the iterator. The indices of the
+    ///    resulting hashes likely will not match the leaf indices before the call the to `par_bridge`.
+    ///
     ///    There are two possibilities to think about:
     ///
-    ///      **a.** Using `iter.enumerate().par_bridge`. This would add extra bookkeeping
-    ///         code to reorder the results to match the original leaf indices.
+    ///      **a.** Calling `enumerate()` before `par_bridge()` (i.e. `iter.enumerate().par_bridge()`).
+    ///         This requires bookkeeping code to reorder the results to match the original
+    ///         leaf indices.
     ///
     ///      **b.** Requiring sorted leaves. There are additional design questions to consider for
     ///         this case:
     ///
-    ///       - How does a naive caller know to sort the leaves prior to calling `MerkleTree::new`?
-    ///         It could be easy to make a mistake.
+    ///       - How would a naive caller know to sort the leaves prior to calling `MerkleTree::new`?
+    ///         This approach seems prone to coding mistakes by callers.
     ///
-    ///       - The caller could pass a sorting `fn` to `MerkleTree::new`. This feels like an
-    ///         unreasonable burden to place on the caller. Additionally, the indices of the
-    ///         resulting `MerkleTree` leaves may not match the indices before the call the to
-    ///         `MerkleTree::new`. It could then be tricky to use `MerkleProof` correctly.
+    ///       - The caller could pass a sorting `Fn` to `MerkleTree::new`. This feels like an
+    ///         unreasonable burden to place on the caller. Since the indices do not match those
+    ///         passed in it still seems prone to coding mistakes by callers.
     #[bench]
     fn bench_blake3_par(bencher: &mut Bencher) {
         let values = gen_blake3_values("a", LEN);

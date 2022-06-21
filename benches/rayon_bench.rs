99c4a67256e1b738a7fa027e8fa01ceb855d7208
GuestMeNot <nadine_m_mckenzie@yahoo.com>
2022-06-21 11:02:43 -0400

    rayon bench docs.

--- benches/rayon_bench.rs
+++ benches/rayon_bench.rs
@@ -17,19 +17,45 @@ mod tests {
         bencher.iter(|| hash_blake3(&values));
     }
 
-    /// This bench shows that around 500 to 1000 iterations that Rayon par_iter() increases performance.
+    /// This bench shows that around 500 to 1000 iterations that Rayon `par_iter()` increases performance.
     ///
-    /// **NOTE**: Naively Hashing [MerkleTree](crate::MerkleTree) leaves using Rayon's par_iter()
-    /// decreases performance compared to a simple `for` loop.
+    /// **NOTE**: The following points illustrate that using parallel processing to increase
+    /// performance of the [MerkleTree](crate::MerkleTree) is tricky:
+    ///
+    /// 1. Rayon's `par_iter` does not work well with slices of undefined length.
+    ///
+    /// 2. Passing a `&Vec` to create the `MerkleTree` rather than a slice or `Iter` dramatically
+    ///    decreased performance when using Rayon's `par_iter()` to create the [MerkleTree] leaf
+    ///    hashes compared to a simple `for` loop
+    ///
+    /// 2. Creating a generic `[T; N]` using a `const` generic parameter to satisfy Rayon's `par_iter`
+    ///    traits requires the caller to know the exact number of leaves at compile time.
+    ///
+    /// 3. Using Rayon's `par_bridge` leaves reorders the results of the original data.
+    ///    There are two possibilities to think about:
+    ///
+    ///      **a.** Using `iter.enumerate().par_bridge`. This would add extra bookkeeping
+    ///         code to reorder the results to match the original leaf indices.
+    ///
+    ///      **b.** Requiring sorted leaves. There are additional design questions to consider for
+    ///         this case:
+    ///
+    ///       - How does a naive caller know to sort the leaves prior to calling `MerkleTree::new`?
+    ///         It could be easy to make a mistake.
+    ///
+    ///       - The caller could pass a sorting `fn` to `MerkleTree::new`. This feels like an
+    ///         unreasonable burden to place on the caller. Additionally, the indices of the
+    ///         resulting `MerkleTree` leaves may not match the indices before the call the to
+    ///         `MerkleTree::new`. It could then be tricky to use `MerkleProof` correctly.
     #[bench]
     fn bench_blake3_par(bencher: &mut Bencher) {
         let values = gen_blake3_values("a", LEN);
         bencher.iter(|| hash_blake3_par(&values));
     }
 
-    /// This bench shows that at around 500 to 1000 iterations that
-    /// there is no performance improvement gained by using for rayon::spawn
-    /// vs using a single thread.
+    /// This bench shows at around 500 to 1000 iterations
+    /// there is no performance improvement gained by using for `rayon::spawn`
+    /// vs. using a single thread.
     #[bench]
     fn bench_blake3_spawn(bencher: &mut Bencher) {
         let values = gen_blake3_values("a", LEN);
